1933fc945e5f567fc237ec0c93f226673affd092
Heei2000 <weijinglun114@gmail.com>
2026-04-15 21:06:07 +0800

    fix: 在 ErrorDemoController 的 divideByZero 方法中进行了 1/0 的除

--- app/Http/Controllers/ErrorDemoController.php
+++ app/Http/Controllers/ErrorDemoController.php
@@ -20,7 +20,7 @@ class ErrorDemoController extends Controller
      */
     public function divideByZero(): Response
     {
-        $value = 1 / 0; // Will throw DivisionByZeroError in PHP 8+
+$value = 0; // Default value when divisor is zero
 
         return response("Result: {$value}");
     }
